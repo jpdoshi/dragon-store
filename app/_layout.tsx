ae6e2153982a5ff839b926b831faa4585d15f51a
jpdoshi <jpdoshi2811@gmail.com>
2025-12-11 13:24:54 +0530

    feat: check network availability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,44 @@
 import "@/global.css";
+import * as Network from "expo-network";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const subscription = Network.addNetworkStateListener((state) => {
+      if (!state.isConnected) {
+        Toast.show({
+          type: "error",
+          text1: "You're Offline",
+          text2: "Please check your internet connection.",
+          autoHide: false,
+          topOffset: 50,
+          swipeable: true,
+        });
+      }
+    });
+
+    return () => {
+      subscription?.remove();
+    };
+  }, []);
+
   return (
-    <Stack
-      screenOptions={{
-        statusBarStyle: "light",
-        headerShown: false,
-        animation: "ios_from_right",
-      }}
-    >
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="appDetails" />
-    </Stack>
+    <>
+      <Stack
+        screenOptions={{
+          statusBarStyle: "light",
+          headerShown: false,
+          animation: "ios_from_right",
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="appDetails" />
+      </Stack>
+
+      {/* Toast container must be inside the root */}
+      <Toast />
+    </>
   );
 }
