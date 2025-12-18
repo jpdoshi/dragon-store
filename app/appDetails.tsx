ce9276aa0a536300ba3c7736db3b2a294c513fd7
jpdoshi <jpdoshi2811@gmail.com>
2025-12-18 18:07:41 +0530

    appDetails: fix UI spacing

--- app/appDetails.tsx
+++ app/appDetails.tsx
@@ -245,7 +245,7 @@ const appDetails = () => {
           entering={FadeIn.delay(150).duration(400)}
           className="px-6"
         >
-          <View className="flex-row gap-5 items-center mb-10">
+          <View className="flex-row gap-5 items-center">
             <Image
               source={config.ICON_REPO_URL + appData.icon}
               style={{ height: 90, width: 90, borderRadius: 24 }}
@@ -270,74 +270,68 @@ const appDetails = () => {
             </View>
           </View>
 
-          <View>
-            <Text className="text-black dark:text-white text-xl font-semibold mb-3">
-              App Info
-            </Text>
+          <View className="h-10" />
 
-            <View className="h-[80px] flex-1 bg-white dark:bg-dark-surface rounded-2xl px-4 flex-row gap-3 items-center justify-around border border-neutral-200 dark:border-neutral-800 shadow-lg shadow-neutral-200 dark:shadow-black">
-              <View className="flex-col gap-1.5 justify-center items-center">
-                <Image
-                  source={require("@/data/assets/android.png")}
-                  style={{ height: 28, width: 28 }}
-                />
-                <Text className="font-medium text-sm text-neutral-500 dark:text-neutral-400">
-                  Platform
-                </Text>
-              </View>
-              <View className="h-10 w-[1px] bg-neutral-300 dark:bg-neutral-700" />
-              {appData.website?.trim() && (
-                <TouchableOpacity
-                  onPress={async () => await openBrowserAsync(appData.website)}
-                  className="flex-col gap-1.5 justify-center items-center"
-                >
-                  <View className="size-7">
-                    <Svg
-                      viewBox="0 0 24 24"
-                      fill={colorScheme == "dark" ? "#2b7fff" : "#51a2ff"}
-                    >
-                      <Path d="M21.721 12.752a9.711 9.711 0 0 0-.945-5.003 12.754 12.754 0 0 1-4.339 2.708 18.991 18.991 0 0 1-.214 4.772 17.165 17.165 0 0 0 5.498-2.477ZM14.634 15.55a17.324 17.324 0 0 0 .332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 0 0 .332 4.647 17.385 17.385 0 0 0 5.268 0ZM9.772 17.119a18.963 18.963 0 0 0 4.456 0A17.182 17.182 0 0 1 12 21.724a17.18 17.18 0 0 1-2.228-4.605ZM7.777 15.23a18.87 18.87 0 0 1-.214-4.774 12.753 12.753 0 0 1-4.34-2.708 9.711 9.711 0 0 0-.944 5.004 17.165 17.165 0 0 0 5.498 2.477ZM21.356 14.752a9.765 9.765 0 0 1-7.478 6.817 18.64 18.64 0 0 0 1.988-4.718 18.627 18.627 0 0 0 5.49-2.098ZM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 0 0 1.988 4.718 9.765 9.765 0 0 1-7.478-6.816ZM13.878 2.43a9.755 9.755 0 0 1 6.116 3.986 11.267 11.267 0 0 1-3.746 2.504 18.63 18.63 0 0 0-2.37-6.49ZM12 2.276a17.152 17.152 0 0 1 2.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0 1 12 2.276ZM10.122 2.43a18.629 18.629 0 0 0-2.37 6.49 11.266 11.266 0 0 1-3.746-2.504 9.754 9.754 0 0 1 6.116-3.985Z" />
-                    </Svg>
-                  </View>
-                  <Text className="font-medium text-sm text-neutral-500 dark:text-neutral-400">
-                    Website
-                  </Text>
-                </TouchableOpacity>
-              )}
-              {appData.website?.trim() && (
-                <View className="h-10 w-[1px] bg-neutral-300 dark:bg-neutral-700" />
-              )}
+          <View className="h-[80px] flex-1 bg-white dark:bg-dark-surface rounded-2xl px-4 flex-row gap-3 items-center justify-around border border-neutral-200 dark:border-neutral-800 shadow-lg shadow-neutral-200 dark:shadow-black">
+            <View className="flex-col gap-1.5 justify-center items-center">
+              <Image
+                source={require("@/data/assets/android.png")}
+                style={{ height: 28, width: 28 }}
+              />
+              <Text className="font-medium text-sm text-neutral-500 dark:text-neutral-400">
+                Platform
+              </Text>
+            </View>
+            <View className="h-10 w-[1px] bg-neutral-300 dark:bg-neutral-700" />
+            {appData.website?.trim() && (
               <TouchableOpacity
-                onPress={async () => {
-                  if (appData.repoUrl.includes("github.com")) {
-                    await openBrowserAsync(
-                      `${appData.repoUrl}/releases/latest`
-                    );
-                  } else {
-                    await openBrowserAsync(appData.repoUrl);
-                  }
-                }}
+                onPress={async () => await openBrowserAsync(appData.website)}
                 className="flex-col gap-1.5 justify-center items-center"
               >
                 <View className="size-7">
                   <Svg
-                    fill="none"
                     viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke={colorScheme == "dark" ? "#fff" : "#000"}
+                    fill={colorScheme == "dark" ? "#2b7fff" : "#51a2ff"}
                   >
-                    <Path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-                    />
+                    <Path d="M21.721 12.752a9.711 9.711 0 0 0-.945-5.003 12.754 12.754 0 0 1-4.339 2.708 18.991 18.991 0 0 1-.214 4.772 17.165 17.165 0 0 0 5.498-2.477ZM14.634 15.55a17.324 17.324 0 0 0 .332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 0 0 .332 4.647 17.385 17.385 0 0 0 5.268 0ZM9.772 17.119a18.963 18.963 0 0 0 4.456 0A17.182 17.182 0 0 1 12 21.724a17.18 17.18 0 0 1-2.228-4.605ZM7.777 15.23a18.87 18.87 0 0 1-.214-4.774 12.753 12.753 0 0 1-4.34-2.708 9.711 9.711 0 0 0-.944 5.004 17.165 17.165 0 0 0 5.498 2.477ZM21.356 14.752a9.765 9.765 0 0 1-7.478 6.817 18.64 18.64 0 0 0 1.988-4.718 18.627 18.627 0 0 0 5.49-2.098ZM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 0 0 1.988 4.718 9.765 9.765 0 0 1-7.478-6.816ZM13.878 2.43a9.755 9.755 0 0 1 6.116 3.986 11.267 11.267 0 0 1-3.746 2.504 18.63 18.63 0 0 0-2.37-6.49ZM12 2.276a17.152 17.152 0 0 1 2.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0 1 12 2.276ZM10.122 2.43a18.629 18.629 0 0 0-2.37 6.49 11.266 11.266 0 0 1-3.746-2.504 9.754 9.754 0 0 1 6.116-3.985Z" />
                   </Svg>
                 </View>
                 <Text className="font-medium text-sm text-neutral-500 dark:text-neutral-400">
-                  Latest APK
+                  Website
                 </Text>
               </TouchableOpacity>
-            </View>
+            )}
+            {appData.website?.trim() && (
+              <View className="h-10 w-[1px] bg-neutral-300 dark:bg-neutral-700" />
+            )}
+            <TouchableOpacity
+              onPress={async () => {
+                if (appData.repoUrl.includes("github.com")) {
+                  await openBrowserAsync(`${appData.repoUrl}/releases/latest`);
+                } else {
+                  await openBrowserAsync(appData.repoUrl);
+                }
+              }}
+              className="flex-col gap-1.5 justify-center items-center"
+            >
+              <View className="size-7">
+                <Svg
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke={colorScheme == "dark" ? "#fff" : "#000"}
+                >
+                  <Path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                  />
+                </Svg>
+              </View>
+              <Text className="font-medium text-sm text-neutral-500 dark:text-neutral-400">
+                Latest APK
+              </Text>
+            </TouchableOpacity>
           </View>
 
           {appData.repoUrl.includes("github.com") && (
