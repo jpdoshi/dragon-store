93030887fe0f2bbbb4b212d8ee15874d914eae85
jpdoshi <jpdoshi2811@gmail.com>
2025-12-18 18:02:03 +0530

    appDetails: add repo button and change source details for github repos

--- app/appDetails.tsx
+++ app/appDetails.tsx
@@ -22,7 +22,7 @@ import {
   View,
 } from "react-native";
 import Animated, { FadeIn } from "react-native-reanimated";
-import Svg, { Path } from "react-native-svg";
+import Svg, { G, Path } from "react-native-svg";
 import Toast from "react-native-toast-message";
 
 const FAVORITES_KEY = "favorite_apps";
@@ -340,6 +340,30 @@ const appDetails = () => {
             </View>
           </View>
 
+          {appData.repoUrl.includes("github.com") && (
+            <TouchableOpacity
+              onPress={async () => await openBrowserAsync(appData.repoUrl)}
+              className="h-[40px] flex-row flex-1 rounded-xl bg-white shadow justify-center items-center gap-1.5 mt-4"
+            >
+              <View className="size-6">
+                <Svg viewBox="0 0 128 128">
+                  <G fill="#000">
+                    <Path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M64 5.103c-33.347 0-60.388 27.035-60.388 60.388 0 26.682 17.303 49.317 41.297 57.303 3.017.56 4.125-1.31 4.125-2.905 0-1.44-.056-6.197-.082-11.243-16.8 3.653-20.345-7.125-20.345-7.125-2.747-6.98-6.705-8.836-6.705-8.836-5.48-3.748.413-3.67.413-3.67 6.063.425 9.257 6.223 9.257 6.223 5.386 9.23 14.127 6.562 17.573 5.02.542-3.903 2.107-6.568 3.834-8.076-13.413-1.525-27.514-6.704-27.514-29.843 0-6.593 2.36-11.98 6.223-16.21-.628-1.52-2.695-7.662.584-15.98 0 0 5.07-1.623 16.61 6.19C53.7 35 58.867 34.327 64 34.304c5.13.023 10.3.694 15.127 2.033 11.526-7.813 16.59-6.19 16.59-6.19 3.287 8.317 1.22 14.46.593 15.98 3.872 4.23 6.215 9.617 6.215 16.21 0 23.194-14.127 28.3-27.574 29.796 2.167 1.874 4.097 5.55 4.097 11.183 0 8.08-.07 14.583-.07 16.572 0 1.607 1.088 3.49 4.148 2.897 23.98-7.994 41.263-30.622 41.263-57.294C124.388 32.14 97.35 5.104 64 5.104z"
+                    ></Path>
+                    <Path d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm2.446 2.729c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zM31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm3.261 3.361c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm4.5 1.951c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm4.943.361c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm4.598-.782c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0"></Path>
+                  </G>
+                </Svg>
+              </View>
+
+              <Text className="text-base text-black font-semibold">
+                Github Repo
+              </Text>
+            </TouchableOpacity>
+          )}
+
           <View className="h-10" />
 
           <View>
@@ -356,9 +380,9 @@ const appDetails = () => {
           )}
 
           {showAPIError && (
-            <Text className="mt-10 text-red-500 text-center font-medium">
-              Sign in with Github to extend API usage limit from 60 to 5000 per
-              hour.
+            <Text className="mt-10 text-emerald-500 text-center font-medium leading-normal">
+              Sign in with Github to extend Github API usage limit from 60 to
+              5000 per hour.
             </Text>
           )}
 
@@ -403,7 +427,7 @@ const appDetails = () => {
                       {repoData?.topics.map((topic: string, index: number) => (
                         <View
                           key={index}
-                          className="border border-rose-500 bg-[rgba(255,32,86,0.1)] rounded px-1"
+                          className="border border-rose-500 bg-[rgba(255,32,86,0.15)] rounded px-1"
                         >
                           <Text className="text-sm font-semibold text-rose-500">
                             {topic}
@@ -481,43 +505,34 @@ const appDetails = () => {
 
           <View className="h-10" />
 
-          <View>
-            <Text className="text-black dark:text-white text-xl font-semibold mb-3">
-              Source Details
-            </Text>
-            <View className="flex-col gap-3">
-              <View className="h-[50px] flex-row gap-3 justify-between items-center border-b border-neutral-200 dark:border-neutral-800">
-                <Text className="text-black dark:text-white font-medium text-base">
-                  Download Source
-                </Text>
-                <Text className="text-green-600 font-semibold text-base">
-                  {appData.repoUrl.includes("github.com") ||
-                  appData.repoUrl.includes("gitlab.com")
-                    ? new URL(appData.repoUrl).hostname
-                    : "Official Website"}
-                </Text>
-              </View>
-              {appData.repoUrl.includes("github.com") && (
+          {!appData.repoUrl.includes("github.com") && (
+            <View>
+              <Text className="text-black dark:text-white text-xl font-semibold mb-3">
+                Source Details
+              </Text>
+              <View className="flex-col gap-3">
                 <View className="h-[50px] flex-row gap-3 justify-between items-center border-b border-neutral-200 dark:border-neutral-800">
                   <Text className="text-black dark:text-white font-medium text-base">
-                    Owner Profile
+                    Download Source
+                  </Text>
+                  <Text className="text-green-600 font-semibold text-base">
+                    Official Website
+                  </Text>
+                </View>
+                <View className="h-[50px] flex-row gap-3 justify-between items-center border-b border-neutral-200 dark:border-neutral-800">
+                  <Text className="text-black dark:text-white font-medium text-base">
+                    Homepage URL
                   </Text>
                   <TouchableOpacity
                     className="flex-row gap-1 items-center"
-                    onPress={async () => {
-                      if (appData.repoUrl.includes("github.com")) {
-                        await openBrowserAsync(
-                          `https://github.com/${appData.owner}`
-                        );
-                      } else {
-                        await openBrowserAsync(
-                          `https://${new URL(appData.repoUrl).hostname}`
-                        );
-                      }
-                    }}
+                    onPress={async () =>
+                      await openBrowserAsync(
+                        `https://${new URL(appData.repoUrl).hostname}`
+                      )
+                    }
                   >
                     <Text className="text-rose-400 font-medium text-base line-clamp-1 text-ellipsis">
-                      {`github.com/${appData.owner}`}
+                      {new URL(appData.repoUrl).hostname}
                     </Text>
                     <View className="size-5">
                       <Svg
@@ -535,52 +550,38 @@ const appDetails = () => {
                     </View>
                   </TouchableOpacity>
                 </View>
-              )}
-              <View className="h-[50px] flex-row gap-3 justify-between items-center border-b border-neutral-200 dark:border-neutral-800">
-                <Text className="text-black dark:text-white font-medium text-base">
-                  {appData.repoUrl.includes("github.com") ||
-                  appData.repoUrl.includes("gitlab.com")
-                    ? "Repo URL"
-                    : "Homepage URL"}
-                </Text>
-                <TouchableOpacity
-                  className="flex-row gap-1 items-center"
-                  onPress={async () => {
-                    if (
-                      appData.repoUrl.includes("github.com") ||
-                      appData.repoUrl.includes("gitlab.com")
-                    ) {
-                      await openBrowserAsync(appData.repoUrl);
-                    } else {
-                      await openBrowserAsync(
-                        `https://${new URL(appData.repoUrl).hostname}`
-                      );
-                    }
-                  }}
-                >
-                  <Text className="text-rose-400 font-medium text-base line-clamp-1 text-ellipsis">
-                    {appData.repoUrl.includes("github.com")
-                      ? appData.repoUrl.replace("https://github.com/", "")
-                      : new URL(appData.repoUrl).hostname}
-                  </Text>
-                  <View className="size-5">
-                    <Svg
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="#ff637e"
-                    >
-                      <Path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-                      />
-                    </Svg>
-                  </View>
-                </TouchableOpacity>
               </View>
             </View>
-          </View>
+          )}
+
+          {!isLoading && repoData && (
+            <View>
+              <Text className="text-black dark:text-white text-xl font-semibold mb-4">
+                Repo Owner
+              </Text>
+              <View className="flex-1 flex-row gap-4 items-center">
+                <Image
+                  source={repoData?.owner.avatar_url}
+                  style={{ height: 75, width: 75, borderRadius: 75 }}
+                />
+                <View>
+                  <Text className="text-black dark:text-white font-medium text-xl">
+                    {repoData?.owner.login}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={async () =>
+                      await openBrowserAsync(repoData?.owner.html_url)
+                    }
+                    className="bg-[rgba(255,32,86,0.15)] rounded-lg py-1 mt-1 px-2 self-start"
+                  >
+                    <Text className="text-rose-500 font-medium text-base">
+                      Check Profile
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          )}
         </Animated.View>
 
         <View className="h-24" />
