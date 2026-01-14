b14d0fc2311db6ba6b232c687cf8280c98455c37
jpdoshi <jpdoshi2811@gmail.com>
2026-01-14 11:57:49 +0530

    config: update URL to target data repo

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 const config = {
   JSON_REPO_URL:
-    "https://raw.githubusercontent.com/jpdoshi/dragon-store/refs/heads/main/data/repo.json",
+    "https://raw.githubusercontent.com/jpdoshi/dragon-store-data/refs/heads/main/data/repo.json",
   ICON_REPO_URL:
-    "https://raw.githubusercontent.com/jpdoshi/dragon-store/refs/heads/main/data/icons/",
+    "https://raw.githubusercontent.com/jpdoshi/dragon-store-data/refs/heads/main/data/icons/",
   DRAGON_STORE_REPO_URL: "https://github.com/jpdoshi/dragon-store",
   GITHUB_CLIENT_ID: process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.EXPO_PUBLIC_GITHUB_CLIENT_SECRET,
